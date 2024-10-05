d04d3124d6447c1c26409bb2396b872dbdd92e97
Renee Yeo <rshu0014@student.monash.edu>
2024-10-06 01:52:15 +0800

    removed donut chart and changed to a stacked bar chart

---
+++ stackedBar.js
@@ -0,0 +1,4 @@
+var stackedBar = "stackedBar.json";
+vegaEmbed("#stackedBar", stackedBar).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
