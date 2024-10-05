94aa5e0995617b12b5292557ffef1833e0bdbac5
Renee Yeo <rshu0014@student.monash.edu>
2024-10-06 02:17:46 +0800

    Updated all graphs

---
+++ choroplethMap.js
@@ -0,0 +1,4 @@
+var choroplethMap = "choroplethMap.json";
+vegaEmbed("#choroplethMap", choroplethMap).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
